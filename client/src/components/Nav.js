e643bdf2efa7380ace8364a3c18e3ff703e4cfd3
Yubraj Nag <69618141+SkyCoder31@users.noreply.github.com>
2023-02-13 17:24:07 +0530

    Init the project

---
+++ client/src/components/Nav.js
@@ -0,0 +1,30 @@
+import whiteLogo from '../images/hotlink_white.png'
+import colorLogo from '../images/hotlink_color.png'
+const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
+    const handleClick = () => {
+      setShowModal(true);
+      setIsSignUp(false);
+    };
+  
+    return (
+      <nav>
+        <div className="logo-container">
+          <img
+            className="logo"
+            src={minimal ? colorLogo : whiteLogo}
+            alt="logo"
+          />
+        </div>
+        {!authToken && !minimal && (
+          <button
+            className="nav-button"
+            onClick={handleClick}
+            disabled={showModal}
+          >
+            Log in
+          </button>
+        )}
+      </nav>
+    );
+  };
+  export default Nav;
